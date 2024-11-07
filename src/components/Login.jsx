25f5a11b5211be4cfa88ab316223e706a8ba7a7e
FajarWnugra <fajarwn474@gmail.com>
2024-11-07 17:44:31 +0700

    edit login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -35,7 +35,7 @@ const Login = () => {
           <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 bg-slate-100 rounded-lg">
             <div className="sm:mx-auto sm:w-full sm:max-w-sm">
               {/* <img className="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company" /> */}
-              <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
+              <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Toko Amanah Listrik</h2>
               <p className="mt-5 text-center text-2l leading-9 tracking-tight text-red-500">{msg}</p>
             </div>
 
